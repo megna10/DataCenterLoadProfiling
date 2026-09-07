47850f7c01849d78818e106ed89a20edbfa514d9
megna10 <megp_15@outlook.com>
2026-09-07 17:55:17 -0400

    Added comments

--- hourly_profile_general_compute.py
+++ hourly_profile_general_compute.py
@@ -11,7 +11,7 @@ SERVER_CONFIGS = {"Standard": {"P_idle": 307, "P_peak": 1133, "num_cpu": 2, "tot
                   }
 
 def get_target_vmid_table():
-    int_active_vms = pd.read_csv('interactive_4cores.csv')
+    int_active_vms = pd.read_csv('general_compute_files/interactive_4cores.csv')
     # grabs every entry in the first column & converts IDs into a set
     target_vm_ids = set(int_active_vms.iloc[:, 0])
 
@@ -42,79 +42,9 @@ def get_target_vmid_table():
 
     # combines all extracted data into one table
     extracted_data = pd.concat(filtered_chunks, ignore_index=True)
-    extracted_data.to_csv('target_vm_rows_1_7.csv', index=False)
+    extracted_data.to_csv('general_compute_files/target_vm_rows_1_7.csv', index=False)
 
     return extracted_data
-
-
-
-def get_min_profiles(df_data, interval_min):
-
-    # filters for day 1
-    first_day_data = df_data[df_data['timestamp'] < END_TIME].copy()
-
-    # rounds every decimal to nearest integer below it to pool cpu_util into hours
-    # adds a new column named hour into data frame
-    first_day_data[f"interval_{interval_min}m"] = (first_day_data['timestamp'] // (interval_min* 60)).astype(int)
-
-    # groups all rows in dataset into 24 distinct buckets
-    # calculate Hourly Average CPU Profile across all selected VMs
-    profile = first_day_data.groupby(f'interval_{interval_min}m')['avg_cpu'].mean().reset_index()
-
-    profile.to_csv(f'hourly_cpu_profiles_{interval_min}.csv', index = False)
-    return profile
-
-
-def get_vm_sections(max_core_count, num_groups):
-    df = pd.read_csv("interactive_4cores.csv")
-
-    df.columns = ["vm_id", "id2", "id3", "start_time", "stop_time", "max_cpu", "min_cpu", "avg_cpu","workload", "cores", "ram"]
-
-    sections = []
-    current_vm_ids = []
-    current_cores = 0
-    current_ram = 0
-    total_group_count = 0
-
-    for _, row in df.iterrows():
-        cores = row["cores"]
-        ram = row["ram"]
-
-        # start a new section if adding this VM would exceed the limit
-        if current_cores + cores > max_core_count:
-            # each section is a dict inside a list
-            sections.append({
-                "vm_ids": current_vm_ids,
-                "total_cores": current_cores,
-                "total_ram": current_ram,
-                })
-
-            total_group_count += 1
-
-            if total_group_count > num_groups:
-                break
-
-            # reset values
-            current_ram = 0
-            current_cores = 0
-            current_vm_ids = []
-
-
-        # update values
-        current_vm_ids.append(row["vm_id"])
-        current_cores += cores
-        current_ram += ram
-
-    # Add the final section / maybe it doesnt matter for now
-    # if current_vm_ids:
-    #     sections.append({
-    #         "vm_ids": current_vm_ids,
-    #         "total_cores": current_cores,
-    #         "total_ram": current_ram
-    #     })
-
-    return sections
-
 
 def get_avg_cpu_utilizations(start_time, end_time, interval_seconds=300, num_vms=100,
     random_seed=42):
@@ -127,7 +57,7 @@ def get_avg_cpu_utilizations(start_time, end_time, interval_seconds=300, num_vms
         24 hours = 288 data points
     """
     # Read the CPU utilization data
-    df = pd.read_csv("target_vm_rows_1_7.csv")
+    df = pd.read_csv("general_compute_files/target_vm_rows_1_7.csv")
 
     # Get unique VM IDs
     vm_ids = df["vm_id"].unique()
@@ -145,7 +75,6 @@ def get_avg_cpu_utilizations(start_time, end_time, interval_seconds=300, num_vms
 
     # Keep ALL measurements belonging to those VMs
     df = df[df["vm_id"].isin(selected_vm_ids)].copy()
-
 
     # Only use timestamps within the requested range
     df = df[
@@ -206,6 +135,7 @@ def calculate_server_power(cpu_profile, num_servers, deployment):
 
     return df[["timestamp", "utilization", "server_power_kw", "it_power_kw"]]
 
+
 def calculate_power_profile(selected_deployment, num_servers):
 
     cpu_profile = get_avg_cpu_utilizations( START_TIME, END_TIME, 300)
@@ -251,110 +181,69 @@ def calculate_power_profile(selected_deployment, num_servers):
 
 # ------------- Plotting --------------------------
 
-def plot_profiles(profile_df, interval_num):
-    """ Plot the main CPU load curve"""
+# def plot_profiles(profile_df, interval_num):
+#     """ Plot the main CPU load curve"""
 
-    plt.plot(
-        profile_df[f'interval_{interval_num}m'],
-        profile_df['avg_cpu'],
-        color='#1f77b4',
-        linewidth=2,
-        marker='o',
-        markersize=3,
-        label=f'{interval_num}-Min Avg CPU Utilization'
-    )
+#     plt.plot(
+#         profile_df[f'interval_{interval_num}m'],
+#         profile_df['avg_cpu'],
+#         color='#1f77b4',
+#         linewidth=2,
+#         marker='o',
+#         markersize=3,
+#         label=f'{interval_num}-Min Avg CPU Utilization'
+#     )
 
-    # Format the X-Axis to display time ticks every 2 hours (every 8 15-min intervals)
-    total_bins = (24 * 60) // interval_num
-    bins_per_hour = 60 // interval_num          # e.g., 12 for 5-min, 4 for 15-min
-    tick_step = bins_per_hour * 2
+#     # Format the X-Axis to display time ticks every 2 hours (every 8 15-min intervals)
+#     total_bins = (24 * 60) // interval_num
+#     bins_per_hour = 60 // interval_num          # e.g., 12 for 5-min, 4 for 15-min
+#     tick_step = bins_per_hour * 2
 
-    tick_intervals = range(0, total_bins + 1, tick_step)
-    tick_labels = [f"{(i * interval_num) // 60:02d}:00" for i in tick_intervals]
+#     tick_intervals = range(0, total_bins + 1, tick_step)
+#     tick_labels = [f"{(i * interval_num) // 60:02d}:00" for i in tick_intervals]
 
-    plt.xticks(ticks=tick_intervals, labels=tick_labels, rotation=0)
-    plt.xlim(0, total_bins - 1)
+#     plt.xticks(ticks=tick_intervals, labels=tick_labels, rotation=0)
+#     plt.xlim(0, total_bins - 1)
 
-    # Axis labels & Title
-    plt.title(f"24-Hour Azure VM CPU Load Profile ({interval_num}-Minute Resolution)", fontsize=14, pad=15)
-    plt.xlabel("Time of Day (HH:MM)", fontsize=11, labelpad=10)
-    plt.ylabel("Average CPU Utilization (%)", fontsize=11, labelpad=10)
+#     # Axis labels & Title
+#     plt.title(f"24-Hour Azure VM CPU Load Profile ({interval_num}-Minute Resolution)", fontsize=14, pad=15)
+#     plt.xlabel("Time of Day (HH:MM)", fontsize=11, labelpad=10)
+#     plt.ylabel("Average CPU Utilization (%)", fontsize=11, labelpad=10)
 
-    # Styling details
-    plt.grid(True, linestyle='--', alpha=0.5)
-    plt.legend(loc='upper right')
-    plt.tight_layout()
+#     # Styling details
+#     plt.grid(True, linestyle='--', alpha=0.5)
+#     plt.legend(loc='upper right')
+#     plt.tight_layout()
 
-    # Display the plot
-    plt.show()
+#     # Display the plot
+#     plt.show()
 
-def plot_overall_cpu_util_sections(section_results):
-    plt.figure(figsize=(12, 6))
+# def plot_overall_cpu_util_sections(section_results):
+#     plt.figure(figsize=(12, 6))
 
-    plt.plot(
-        section_results["timestamp"],
-        section_results["overall_avg_cpu"],
-        linewidth=2,
-        marker="o",
-        markersize=3
-    )
+#     plt.plot(
+#         section_results["timestamp"],
+#         section_results["overall_avg_cpu"],
+#         linewidth=2,
+#         marker="o",
+#         markersize=3
+#     )
 
 
-    plt.xlabel("Time (seconds)")
-    plt.ylabel("Average CPU Utilization (%)")
-    plt.title("Overall CPU Utilization Over Time")
+#     plt.xlabel("Time (seconds)")
+#     plt.ylabel("Average CPU Utilization (%)")
+#     plt.title("Overall CPU Utilization Over Time")
 
-    plt.grid(True)
-    plt.show()
+#     plt.grid(True)
+#     plt.show()
 
 
 def main():
     """Main execution block where workflow functions are called."""
-    # vm_id_table = get_target_vmid_table()
-    # time_interval = 10
-    # df_input = pd.read_csv('target_vm_rows_1_7.csv')
-    # profile_df = get_min_profiles(df_input, time_interval)
-    # plot_profiles(profile_df, time_interval)
-
-    # sects_of_ids = get_vm_sections(64, 10)
-    # df = get_avg_cpu_utilizations(sects_of_ids, 86400 , 86400 * 2 )
-    # plot_overall_cpu_util_sections(get_overall_avg_cpu(df))
 
     power_profile = calculate_power_profile(
         selected_deployment="Standard",
         num_servers=10
-    )
-
-    print("\n========== POWER PROFILE CHECK ==========")
-
-    print("\nFirst 10 rows:")
-    print(power_profile.head(10))
-
-    print("\nLast 10 rows:")
-    print(power_profile.tail(10))
-
-    print("\nNumber of rows:")
-    print(len(power_profile))
-
-    print("\nHour range:")
-    print(
-        power_profile["hour"].min(),
-        "to",
-        power_profile["hour"].max()
-    )
-
-    print("\nUtilization range:")
-    print(
-        power_profile["utilization"].min(),
-        "to",
-        power_profile["utilization"].max()
-    )
-
-    print("\nPower range:")
-    print(
-        power_profile["it_power_kw"].min(),
-        "to",
-        power_profile["it_power_kw"].max(),
     )
 
 if __name__ == "__main__":
